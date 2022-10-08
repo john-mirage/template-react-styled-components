d0e79a7def689c87ddca1fe75e8acc82da498e6d
john-mirage <chaosdynamix@posteo.net>
2022-10-08 20:42:14 +0200

    ref: switch to testing library for react tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
@@ -16,6 +19,15 @@ export default defineConfig({
     alias: {
       "@components": resolve(__dirname, "src/components"),
       "@images": resolve(__dirname, "src/images"),
+      "@test": resolve(__dirname, "src/test"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    //css: true,
   },
 });
