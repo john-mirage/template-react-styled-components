4d9e88bc898a1bcde0428578ab32cfcad3d4b750
john-mirage <chaosdynamix@posteo.net>
2022-10-08 21:50:31 +0200

    ref: move vitest config in its own file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/repository-name/",
   plugins: [
@@ -22,13 +18,5 @@ export default defineConfig({
       "@test": resolve(__dirname, "src/test"),
       "@utils": resolve(__dirname, "src/utils"),
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
-    //css: true,
   },
 });
