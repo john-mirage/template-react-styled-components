a9e644276f5e1a9c05890dedd2f2a63773f48e32
john-mirage <28307090+john-mirage@users.noreply.github.com>
2022-11-02 19:31:39 +0100

    feat: add styles folder absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/repository-name/",
   plugins: [
     react({
       babel: {
@@ -17,6 +16,7 @@ export default defineConfig({
     alias: {
       "@components": resolve(__dirname, "src/components"),
       "@images": resolve(__dirname, "src/images"),
+      "@styles": resolve(__dirname, "src/styles"),
       "@test": resolve(__dirname, "src/test"),
       "@utils": resolve(__dirname, "src/utils"),
     },
